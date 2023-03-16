f29534cf018c2a2cee9a8c71a18cd42760e2896e
Tarunkumarsaini <123304632+Tarunkumarsaini@users.noreply.github.com>
2023-03-16 17:06:09 +0530

    updated

---
+++ first.js
@@ -0,0 +1,40 @@
+const prompt = require("prompt-sync")();
+//var fname = prompt("what is your first name ::");
+//var lname = prompt("what is your last name  ::");
+//console.log(" your full name is ::" , fname , lname);
+let a=56;
+console.log(a);
+//primitive data types( nn bb ss u )
+let b = null;
+let c = true;
+let d = BigInt("58649");
+let e = "tarun";
+let f = Symbol("i am a nice symbol");
+let g ;
+console.log(a,b,c,d,f,g);
+
+//objects in java script
+const item={
+    "tarun":true,
+    "saini":false,
+    "varun":4545,
+    "arun":undefined
+}
+console.log(item["saini"]);
+console.log(item["tarun"]);
+console.log(item["varun"]);
+console.log(item["arun"]);
+console.log(item["saini"]);
+console.log(item.tarun);
+ //operators in javascript
+
+ let aa =45;
+ let bb = 4;
+ console.log("aa+bb = " , aa+bb);
+ console.log("aa-bb = " , aa-bb);
+ console.log("aa/bb = " , aa/bb);
+ console.log("aa*bb = " , aa*bb);
+ console.log("aa%bb = " , aa%bb);
+ console.log("aa**bb = " , aa**bb);
+
+
