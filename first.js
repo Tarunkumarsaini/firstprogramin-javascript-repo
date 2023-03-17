f6926873787a6e6a4ee3fc15541ddd1e00897d01
Tarunkumarsaini <123304632+Tarunkumarsaini@users.noreply.github.com>
2023-03-17 17:15:34 +0530

    practise

--- first.js
+++ first.js
@@ -1,7 +1,8 @@
 const prompt = require("prompt-sync")();
-//var fname = prompt("what is your first name ::");
-//var lname = prompt("what is your last name  ::");
-//console.log(" your full name is ::" , fname , lname);
+/*
+var fname = prompt("what is your first name ::");
+var lname = prompt("what is your last name  ::");
+console.log(" your full name is ::" , fname , lname);
 let a=56;
 console.log(a);
 //primitive data types( nn bb ss u )
@@ -29,12 +30,81 @@ console.log(item.tarun);
  //operators in javascript
 
  let aa =45;
- let bb = 4;
+ let bb = 4;/
  console.log("aa+bb = " , aa+bb);
  console.log("aa-bb = " , aa-bb);
  console.log("aa/bb = " , aa/bb);
  console.log("aa*bb = " , aa*bb);
  console.log("aa%bb = " , aa%bb);
  console.log("aa**bb = " , aa**bb);
+ 
+ let num = prompt("what is ypur age ::::");
+  num=Number.parseInt(num)
+  if(num==18){
+    alert("this is a valid age ");
+ }
+ else if(num>18 && num<25){
+    alert("you are a intermediate");
+ }
+  else{
+    alert("this is a invalid age");
+ }
+//   loops in javascript
+// for in loop
+let obj={
+    tarun:89,arun:78,aman:67,ansuman:78
+}
+for(let a in obj){
+    console.log("marks of "+a+" are "+obj[a]);
+}
+// for of loop
+for(let b of "tarun"){
+    console.log(b);
+
+}
+// funstions in javascript
+function sum(x,y){
+    return x+y;
+}
+const subtraction = (p,q)=>{
+    return p-q;
+}
+let a, b;
+a=prompt("enter a first number for ::");
+a=Number.parseInt(a);
+b=prompt("enter a second number for ::");
+b=Number.parseInt(b)
+console.log("your sum of to given number are ::"+sum(a,b));
+console.log("your subtraction  to given number are ::"+subtraction(a,b));
+
+// strings
+let name = "tarun";
+console.log(name.length);
+for(let i = 0;i<name.length;i++){
+console.log(name[i]);
+};
+
+// Template literals
+let boy1="tarun";
+let boy2="arun";
+let sentance=`${boy1} is a friend of ${boy2}`;
+console.log(sentance); 
+
+// Escape sequence characters
+let fruit = 'banna\'na\n';
+console.log(fruit);
+console.log(fruit.length);*/
+
+// string methods
+let name= "         tarun      kumar    saini    ";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(4));
+console.log(name.replace("ta","r"));
+console.log(name.concat(" is a bodybuilder", name , "good person "));
+console.log(name.trim());
+
+
 
 
